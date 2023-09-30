9a17fcd6bbfae66ac05d7f7816029e74e9e83208
Roman Shoshkin <anoningyou@gmail.com>
2023-09-30 10:29:37 -0300

    App done

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,8 @@
 import { Component } from '@angular/core';
+import { ToastrService } from 'ngx-toastr';
+
+declare var require: any;
+const {XMLParser, XMLBuilder, XMLValidator} = require('fast-xml-parser');
 
 @Component({
   selector: 'app-root',
@@ -7,4 +11,80 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'PointEditor';
+  file: any;
+  fileName: string = ""
+  displayedColumns: string[] = ["name", "lat", "lon"]
+
+  constructor(private toastr: ToastrService) {}
+
+  onFileSelected() {
+    const inputNode: any = document.querySelector('#file');
+  
+    if (typeof (FileReader) !== 'undefined') {
+      const reader = new FileReader();
+
+      reader.onload = (e: any) => {
+        let xml = e.target.result;
+        const options = {
+          ignoreAttributes : false
+        };
+        const parser = new XMLParser(options);
+        const data = parser.parse(xml);
+        if (!data.gpx) {
+          this.toastr.error("File isn't gpx")
+          return;
+        }
+
+        if (!this.file){
+          this.file = data;
+          if (!this.file.gpx.wpt)
+            this.file.gpx.wpt = [];
+          this.toastr.success(`New file was loaded`);
+        }
+        else if (data.gpx?.wpt?.length){
+          const oldList = this.file.gpx.wpt as any[];
+          const newList = data.gpx.wpt as any[];
+          const pointsToAdd = newList.filter(p => 
+            !oldList.find(w => 
+                 w["name"] === p["name"] 
+              && w["@_lat"] === p["@_lat"] 
+              && w["@_lon"] === p["@_lon"])
+          );
+          if (pointsToAdd.length){
+            this.file.gpx.wpt = oldList.concat(pointsToAdd);
+            this.toastr.success(`${pointsToAdd.length} points were added`);
+          }
+          else
+            this.toastr.info("File has no new points");
+        }
+        else {
+          this.toastr.info("File has no points");
+        }
+      }
+      reader.readAsText(inputNode.files[0])
+    }
+  }
+
+  onClean(){
+    this.file = null;
+  }
+
+  onSave() {
+    const options = {
+      ignoreAttributes : false
+    };
+    
+    const builder = new XMLBuilder(options);
+    let xmlDataStr = builder.build(this.file);
+    let element = document.createElement('a');
+    element.setAttribute('href', 'data:text/xml;charset=utf-8,' + encodeURIComponent(xmlDataStr));
+    element.setAttribute('download', `${this.fileName}.gpx`);
+  
+    element.style.display = 'none';
+    document.body.appendChild(element);
+  
+    element.click();
+  
+    document.body.removeChild(element);
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,34 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {MatCardModule} from '@angular/material/card';
+import {MatButtonModule} from '@angular/material/button';
+import {MatToolbarModule} from '@angular/material/toolbar';
+import {MatInputModule} from '@angular/material/input';
+import { ToastrModule } from 'ngx-toastr';
+import {FormsModule, ReactiveFormsModule} from '@angular/forms';
+import {MatIconModule} from '@angular/material/icon';
+import {MatTableModule} from '@angular/material/table';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    MatCardModule,
+    MatButtonModule,
+    MatToolbarModule,
+    MatInputModule,
+    ToastrModule.forRoot({
+      positionClass: 'toast-bottom-right'
+    }),
+    FormsModule,
+    ReactiveFormsModule,
+    MatIconModule,
+    MatTableModule
   ],
   providers: [],
   bootstrap: [AppComponent]
